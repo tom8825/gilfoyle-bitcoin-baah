f2a73a69dc74b9e04dde06ef98a5fa8008c5e202
Tom <tlparker1988@gmail.com>
2019-04-26 08:38:37 +0100

    see notes
    added:
    json parse
    user interaction button
    change checking function

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+var oldValue;
+var newValue;
 
+$(document).ready(getValue());
 
-function getValue(){
+function getValue() {
     $.ajax({
         url: "https://api.coindesk.com/v1/bpi/currentprice.json",
         cache: false,
-        success: function(html){
-          $("#results").append(html);
+        success: function (html) {
+            var obj = $.parseJSON(html);
+            var rate = obj.bpi.GBP.rate_float;
+            newValue = rate;
+            
+            $("#results").html('<span>'+rate+'</span>');
         }
-      });
+    });
 }
-    
+
+window.setInterval(function () {
+    getValue();
+    checkChange();
+}, 5000);
+
+function checkChange(){
+    var audio = new Audio('baaah.wav');
+    if(newValue !== oldValue){
+        if(newValue > oldValue){
+            $("#results span").css('background', 'green')
+        }else{
+            $("#results span").css('background', 'red')
+        }
+        oldValue = newValue;
+        audio.play();
+    }
+}
